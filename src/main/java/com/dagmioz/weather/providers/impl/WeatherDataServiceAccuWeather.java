2f41693c8fd6b706ac5959d4d0856e6f41af2d7b
Oz Dagmi <dagmioz@gmail.com>
2016-07-18 11:10:04 +0300

    add service accu weather url for location searching

--- src/main/java/com/dagmioz/weather/providers/impl/WeatherDataServiceAccuWeather.java
+++ src/main/java/com/dagmioz/weather/providers/impl/WeatherDataServiceAccuWeather.java
@@ -3,5 +3,22 @@ package com.dagmioz.weather.providers.impl;
 /**
  * Created by oz on 18/07/16.
  */
-public class WeatherDataServiceAccuWeather {
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.dagmioz.weather.ReadDataFromJson;
+import com.dagmioz.weather.WeatherDataServiceException;
+import com.dagmioz.weather.model.Location;
+import com.dagmioz.weather.model.WeatherData;
+import com.dagmioz.weather.providers.api.IWeatherDataService;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class WeatherDataServiceAccuWeather implements IWeatherDataService {
+
+    private final static String SERVICE_URL = "dataservice.accuweather.com/locations/v1/search?apikey=paPyuYGUKqxE6ALLMRvq7YT9XZ20b0BJ&language=en-us&q=";
+
 }
+
