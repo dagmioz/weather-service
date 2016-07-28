9fbdca0a0b7535fdf8cbe175c71921fc639f37bf
Oz Dagmi <dagmioz@gmail.com>
2016-07-28 11:36:15 +0300

    add set and get method for String type on WeatherData , for failed city search in Wundersround api

--- src/main/java/com/dagmioz/weather/model/WeatherData.java
+++ src/main/java/com/dagmioz/weather/model/WeatherData.java
@@ -25,7 +25,8 @@ public class WeatherData implements Serializable {
 
     //objects declaration for Wunderground provider
     private String type;
-
+    public String getType() {return type;}
+    public void setType(String type) {this.type = type;}
 
 
     public String getCoord1() {
