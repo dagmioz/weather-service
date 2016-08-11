d6ece7f04919401d098a48400b04b4f58abef130
Idan Entin <idanen@gmail.com>
2016-08-11 11:11:36 +0300

    First commit for unit testing

---
+++ src/test/java/com/dagmioz/weather/providers/WeatherDataServiceTest.java
@@ -0,0 +1,10 @@
+package com.dagmioz.weather.providers;
+
+/**
+ * Created by idanen on 11/08/2016.
+ */
+public class WeatherDataServiceTest {
+    public void testDataProvider() {
+        // TODO: to start testing we need to find a way to mock ReadDataFromJson so we won't actually go out to the network
+    }
+}
